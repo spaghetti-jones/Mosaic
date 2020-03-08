0e6b0b318e8bc3b0a0a3d6318940134fa083b66a
Luis Barrera <luisbarrera0626@yahoo.com>
2020-03-07 18:08:14 -0600

    add files again again again

--- Mosaic.java
+++ Mosaic.java
@@ -1,6 +1,7 @@
 public class Mosaic {
 
     public static void main(String[] args) {
+        System.out.println("Mosaic start...");
         MosaicFrame frame = new MosaicFrame();
         frame.setVisible(true);
     }
