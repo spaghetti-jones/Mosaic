71e54a91479557d2644fa5896179c80432a2bce4
Luis Barrera <luisbarrera0626@yahoo.com>
2020-03-07 18:21:48 -0600

    add files FINAL

--- Mosaic.java
+++ Mosaic.java
@@ -1,3 +1,13 @@
+/**
+ * @author Luis Barrera
+ * 
+ * Eric Pogue is the author of:
+ * the contrasting code equation
+ * the Utils.java (int GetNumberBetween) 
+ * 
+ * In-class code for randomly selecting letter from string used
+ */
+
 public class Mosaic {
 
     public static void main(String[] args) {
